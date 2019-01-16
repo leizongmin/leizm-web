53ec6c8fc344d35c3e1a352648387fd03c2e9250
Zongmin Lei <leizongmin@gmail.com>
2019-01-16 11:41:33 +0800

    fix: component.proxy

--- src/lib/component/proxy.ts
+++ src/lib/component/proxy.ts
@@ -3,6 +3,7 @@
  * @author Zongmin Lei <leizongmin@gmail.com>
  */
 
+import * as path from "path";
 import { Context } from "../context";
 import { MiddlewareHandle } from "../define";
 import { proxyRequest, ProxyTarget, parseProxyTarget } from "../module/proxy.request";
@@ -35,7 +36,7 @@ export function proxy(options: ProxyOptions): MiddlewareHandle<Context> {
     }
     const target: ProxyTarget = {
       ...baseTarget,
-      path: baseTarget.path + ctx.request.url,
+      path: path.join(baseTarget.path, ctx.request.url),
       headers: { ...originalHeaders, ...baseTarget.headers },
     };
     proxyRequest(ctx.request.req, ctx.response.res, target).catch(err => {
